4aa455e143032e3374a43fb9213c3bd6cca97a56
Javier Sierra <javess@fb.com>
2016-03-21 13:46:33 +0000

    Initial Check in

---
+++ Assets/Scripts/PlayerAssets/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using VoiceChat;
+using VoiceChat.Networking;
+using VoiceChat.Demo;
+
+public class PlayerController : NetworkBehaviour
+{
+    public GameObject bulletPrefab;
+    public GameObject voiceChatPrefab;
+    public Transform bulletSpawn;
+    public int bulletSpeed = 6;
+
+    private GameObject voiceChat = null;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CmdFire();
+        }
+    }
+
+    [Command]
+    void CmdFire()
+    {
+        // Create the Bullet from the Bullet Prefab
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab.gameObject,
+            bulletSpawn.position,
+            bulletSpawn.rotation);
+        var rigidBody = bullet.GetComponent<Rigidbody>();
+        // Add velocity to the bullet
+        rigidBody.velocity = rigidBody.transform.forward * bulletSpeed;
+
+        // Spawn the bullet on the Clients
+        NetworkServer.Spawn(bullet);
+
+        // Destroy the bullet after 2 seconds
+        Destroy(bullet, 2.0f);
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.blue;
+        if (isClient) { 
+            voiceChat = Instantiate<GameObject>(voiceChatPrefab.gameObject);
+            voiceChat.transform.SetParent(transform);
+			gameObject.AddComponent<VoiceChatUi>();
+        }
+    }
+}
